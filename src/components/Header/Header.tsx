bd3b961a4e345df4e2898c33b840d6cb1e80508c
eabz <eabz@kindynos.mx>
2025-03-22 12:22:23 -0600

    Fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,11 @@
 'use client'
 
-import { HStack, Stack, useBreakpointValue } from '@chakra-ui/react'
+import { Box, HStack, Stack, useBreakpointValue } from '@chakra-ui/react'
 import { useState } from 'react'
 import { HeaderLogo } from './Logo'
 import { MenuButton } from './MenuButton'
 import { MenuLink } from './MenuLink'
 import { Sidebar } from './Sidebar'
-import { TwitterButton } from './SocialButtons'
 
 export const MENU_LINKS = [
   { href: '/', label: 'HOME' },
@@ -21,9 +20,21 @@ export function Header() {
   const mobile = useBreakpointValue({ base: true, lg: false })
 
   return (
-    <Stack position="absolute" top={0} w="full" zIndex={100} m={0}>
-      <Sidebar open={open} close={() => setOpen(false)} />
-      <HStack justify="space-between" mx={10} my={2} h="80px" align="center">
+    <Stack width="full" m={0}>
+      <Box position="absolute" width="full">
+        <Sidebar open={open} close={() => setOpen(false)} />
+      </Box>
+      <HStack
+        justify="space-between"
+        px={10}
+        my={2}
+        h="80px"
+        align="center"
+        position="absolute"
+        top="0"
+        zIndex={100}
+        width="100dvw"
+      >
         <HeaderLogo />
         <HStack justify="end" w="full" align="center" spaceX={4}>
           {mobile ? (
@@ -31,7 +42,6 @@ export function Header() {
           ) : (
             MENU_LINKS.map((link) => <MenuLink key={link.href} label={link.label} href={link.href} />)
           )}
-          <TwitterButton />
         </HStack>
       </HStack>
     </Stack>
