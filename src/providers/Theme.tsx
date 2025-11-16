abb180cbece523d94a071c27f27917a9c06e1b4c
0xeabz <eabz@kindynos.mx>
2025-11-16 15:02:44 -0600

    Simplify

--- src/providers/Theme.tsx
+++ src/providers/Theme.tsx
@@ -1,18 +1,9 @@
 'use client'
 
 import { ChakraProvider } from '@chakra-ui/react'
-import { CacheProvider } from '@emotion/react'
 import type { ReactNode } from 'react'
-import { useMemo } from 'react'
-import { createEmotionCache } from '@/lib/emotion-cache'
 import { SYSTEM } from '@/theme'
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  const cache = useMemo(() => createEmotionCache(), [])
-
-  return (
-    <CacheProvider value={cache}>
-      <ChakraProvider value={SYSTEM}>{children}</ChakraProvider>
-    </CacheProvider>
-  )
+  return <ChakraProvider value={SYSTEM}>{children}</ChakraProvider>
 }
